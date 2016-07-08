4b169c9f702051b49f1e1e3c7748c278c73901c7
ilya osovets <iosovets@gmail.com>
2016-07-07 22:14:02 -0700

    npm installed stuff

--- app.js
+++ app.js
@@ -1,26 +1,17 @@
-var express = require('express');
-//linking the express module
-// var request = require('request');
-// requires request node modules
-var app = express();
-//app instance of express
-var db =
+var express = require('express'); //linking the express module
+var app     = express(); //app instance of express
+var db      = require('./db'); // added db
+var port    = process.env.PORT || 3000; //  sets the listining port
+// var linkrouter = require('./routes/links'); //router page
 
 
 
+// app.get('/', function(req,res){
+//   res.json({message:'hello, world!'});
+// });
 
-var port = process.env.PORT || 3000;
-//  sets the listining port
-var linkrouter = require('./routes/links');
+//   app.use('/links', linkrouter);
 
-
-
-app.get('/', function(req,res){
-  res.json({message:'hello, world!'});
-});
-
-  app.use('/links', linkrouter);
-// get reddit if routes start with reddit
 
 app.listen(port, function(){
   console.log ('Server listing on port '+port);
