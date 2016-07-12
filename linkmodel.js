68b9a173dc0394b2aa4528a461902e691d24ca91
ilya osovets <iosovets@gmail.com>
2016-07-11 21:06:46 -0700

    added model

--- linkmodel.js
+++ linkmodel.js
@@ -1,1 +1,15 @@
-linkmodel.js
+var mongoose = require('mongoose');
+
+
+
+var linkSchema = new mongoose.Schema(
+          link: String,
+          title:String
+
+);
+
+
+var Linklist = mongoose.model('linklist', linkSchema);
+
+
+module.exports = Linklist;
