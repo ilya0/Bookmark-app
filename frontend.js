f7a05c80f3961f386f2553c1ffb98aa0defe356e
ilya osovets <iosovets@gmail.com>
2016-07-12 20:11:53 -0700

    added ajax to front end and added title input

--- frontend.js
+++ frontend.js
@@ -3,16 +3,34 @@ console.log("js frontend script connected");
 
 $( document ).ready(function(){ //on ready command
 
-$("#inputbutton").click(function(){ //on button click do this following stuff
-  $(".ulclass").append("<li>" +inputboxvalue+ "</li>" );
-
-  var inputboxvalue = $('#inputbox').val();
-  console.log(inputboxvalue);
-
-
+    $("#inputbutton").click(function(){ //on button click do this following stuff
+       var inputboxvalue = $('#inputbox').val();
+       var titleboxvalue = $('#titlebox').val();
+      $(".ulclass").append("<li>" +inputboxvalue+ "</li>" );
+        console.log(inputboxvalue); //console log the value
 
 
-});
+         $.ajax({ //ajax post request
+          method: "POST", //post
+          url: "/api", //api url
+          data: {link: inputboxvalue,
+            title: titlebox
+          }, //data is textboxinfo var but it needs to be in object format for the ajax data
+          error: function( response ){ //if error do this
+            console.log("There was a problemmo.");
+          }
+        })
+      .done(function( textboxinfo ){ //on done do this function
+        console.log(textboxinfo); // console log var textboxinfo
+       alert( "Data Saved: " + textboxinfo ); //alert textbox var
+
+
+      });
+
+
+
+
+  });
 
 
 });
