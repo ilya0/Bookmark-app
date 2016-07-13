cb8b5ec4a8e7787d486e79668cc2773388dfb50b
ilya osovets <iosovets@gmail.com>
2016-07-12 19:26:07 -0700

    frontend working

--- frontend.js
+++ frontend.js
@@ -1,2 +1,24 @@
 // frontend.js
-console.log("js connected");
+console.log("js frontend script connected");
+
+$( document ).ready(function(){ //on ready command
+
+$("#inputbutton").click(function(){ //on button click do this following stuff
+  $(".ulclass").append("<li>" +inputboxvalue+ "</li>" );
+
+  var inputboxvalue = $('#inputbox').val();
+  console.log(inputboxvalue);
+
+
+
+
+});
+
+
+});
+
+
+
+
+
+
