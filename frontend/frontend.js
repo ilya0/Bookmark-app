831c728cafb2f5cf8ed42ae951b67170e62e06d0
ilya osovets <iosovets@gmail.com>
2016-07-18 08:04:10 -0700

    working on front end in html and csss

--- frontend/frontend.js
+++ frontend/frontend.js
@@ -4,30 +4,33 @@ console.log("js frontend script connected");
 $( document ).ready(function(){ //on ready command
 
     $("#inputbutton").click(function(){ //on button click do this following stuff
-       var inputboxvalue = $('#inputbox').val();
+       var linkaddressvalue = $('#linkaddressbox').val();
        var titleboxvalue = $('#titlebox').val();
 
-      $(".ulclass").append("<li>" +titleboxvalue+" "+inputboxvalue+ "</li>" );
-        console.log(inputboxvalue); //console log theut value
+
+      $(".ulclass").append("<li>" +titleboxvalue+" "+linkaddressvalue+ "</li>"  );
+        console.log(linkaddressvalue); //console log theut value
         console.log(titleboxvalue); //console log theut value
 
 
-      //    $.ajax({ //ajax post request
-      //     method: "POST", //post
-      //     url: "/api", //api url
-      //     data: {link: inputboxvalue,
-      //       title: titlebox
-      //     }, //data is textboxinfo var but it needs to be in object format for the ajax data
-      //     error: function( response ){ //if error do this
-      //       console.log("There was a problemmo.");
-      //     }
-      //   })
-      // .done(function( textboxinfo ){ //on done do this function
-      //   console.log(textboxinfo); // console log var textboxinfo
-      //  alert( "Data Saved: " + textboxinfo ); //alert textbox var
+         $.ajax({ //ajax post request
+          method: "POST", //post
+          url: "/links", //api url
+          data: {
+            link: linkaddressvalue,
+            title: titlebox
+          }, //data is textboxinfo var but it needs to be in object format for the ajax data
+          error: function( response ){ //if error do this
+            console.log("There was a problemmo.");
+          }
+        })
+
+      .done(function( textboxinfo ){ //on done do this function
+        console.log(textboxinfo); // console log var textboxinfo
+       alert( "Data Saved: " + textboxinfo ); //alert textbox var
 
 
-      // });
+      });
 
 
 
