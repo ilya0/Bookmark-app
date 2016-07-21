f633ea673765ea97aff36521a82b43dffbee5cf2
ilya osovets <iosovets@gmail.com>
2016-07-20 17:45:26 -0700

    tried the crossDomain snippit

--- frontend/frontend.js
+++ frontend/frontend.js
@@ -1,7 +1,18 @@
 // frontend.js
 console.log("js frontend script connected");
+var allowCrossDomain = function(req, res, next) {
+   res.header('Access-Control-Allow-Origin', '*');
+   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+   res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+   next();
+};
+
 
 $( document ).ready(function(){ //on ready command
+
+
+
 
     $("#inputbutton").click(function(){ //on button click do this following stuff
        var linkaddressvalue = $('#linkaddressbox').val();
@@ -13,12 +24,25 @@ $( document ).ready(function(){ //on ready command
         console.log(titleboxvalue); //console log theut value
 
 
+        pushdata(linkaddressvalue,titleboxvalue);
+        console.log("pushdata ran");
+
+
+
+
+  });
+
+
+});
+
+function pushdata(linkaddressvalue,titleboxvalue){
          $.ajax({ //ajax post request
           method: "POST", //post
+          crossDomain: false,
           url: "/links", //api url
           data: {
             link: linkaddressvalue,
-            title: titlebox
+            title: titleboxvalue
           }, //data is textboxinfo var but it needs to be in object format for the ajax data
           error: function( response ){ //if error do this
             console.log("There was a problemmo.");
@@ -30,18 +54,16 @@ $( document ).ready(function(){ //on ready command
        alert( "Data Saved: " + textboxinfo ); //alert textbox var
 
 
-      });
+    });
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "http://www.example.org/ajax.php", true);
+xhr.setRequestHeader("X-My-Custom-Header", "some value");
+xhr.onload = function () {
+    console.log(xhr.responseText);
+};
+xhr.send();
 
-
-
-
-  });
-
-
-});
-
-
-
+};
 
 
 
