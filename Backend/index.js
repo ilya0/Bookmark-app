20dc56843440947f8c8b82784c2a76b0181536d5
ilya osovets <iosovets@gmail.com>
2016-07-24 11:01:33 -0700

    morning commit

--- Backend/index.js
+++ Backend/index.js
@@ -10,6 +10,15 @@ var router        = express.Router(); //simplifying the router
 var mongoose      = require('mongoose');
 var logger        = require( 'morgan' ); //logs the shit into console
 
+///cross domain working?
+var allowCrossDomain = function(req, res, next) {
+   res.header('Access-Control-Allow-Origin', '*');
+   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+   res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+   next();
+};
+
 
 // Use middleware
 app.use( logger( 'dev' ) );
--- frontend/frontend.js
+++ frontend/frontend.js
@@ -1,12 +1,5 @@
 // frontend.js
 console.log("js frontend script connected");
-var allowCrossDomain = function(req, res, next) {
-   res.header('Access-Control-Allow-Origin', '*');
-   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-   res.header('Access-Control-Allow-Headers', 'Content-Type');
-
-   next();
-};
 
 
 $( document ).ready(function(){ //on ready command
